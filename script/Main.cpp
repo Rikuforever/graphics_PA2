7b82d10839344c2066ee057bbd0624d80ddab0d5
HoJoong Lee <rikuforever@korea.ac.kr>
2018-04-11 15:19:04 +0900

    [Done] Load ".obj" File

--- script/Main.cpp
+++ script/Main.cpp
@@ -8,21 +8,27 @@ using namespace std;
 
 bool DepthTest = true;
 
+Mesh M_teapot;
+Mesh M_torus;
 void MeshLoad()
 {
 	//To do
-
-
-
+	// Load Meshes
+	M_teapot.LoadMesh("teapot2.obj");
+	M_torus.LoadMesh("torus.obj");
 
 }
 
 void ComputeNormal()
 {
 	//To do
-
-
-
+	// Compute Normals
+	M_teapot.FindNeighborFaceArray();
+	M_teapot.ComputeVertexNormal();
+	M_teapot.ComputeFaceNormal();
+	M_torus.FindNeighborFaceArray();
+	M_torus.ComputeFaceNormal();
+	M_torus.ComputeVertexNormal();
 
 }
 
@@ -58,6 +64,23 @@ void RenderPlane()
 	glEnd();	
 }
 
+void RenderMesh(Mesh m) {
+	for (int i = 0; i < m.faceArray.size(); i++) {
+		Face f = m.faceArray[i];
+		VECTOR3D n = f.normal;
+		VECTOR3D v0 = m.vertexArray[f.vertex0].position;
+		VECTOR3D v1 = m.vertexArray[f.vertex1].position;
+		VECTOR3D v2 = m.vertexArray[f.vertex2].position;
+
+		glBegin(GL_TRIANGLES);
+		glColor3f(1.0, 0.0, 0.0);
+		glNormal3f(n.x, n.y, n.z);
+		glVertex3f(v0.x, v0.y, v0.z);
+		glVertex3f(v1.x, v1.y, v1.z);
+		glVertex3f(v2.x, v2.y, v2.z);
+		glEnd();
+	}
+}
 
 void Rendering(void)
 {
@@ -69,8 +92,8 @@ void Rendering(void)
 	glLoadIdentity();
 	gluLookAt(0.0f, 20.0f, 30.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 	//To Do
-
-
+	RenderMesh(M_teapot);
+	RenderMesh(M_torus);
 
 
 
